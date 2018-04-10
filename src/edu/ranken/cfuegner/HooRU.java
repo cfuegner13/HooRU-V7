1c6454d88a008d823519cec4b58f02075f05c088
Colin Fuegner <cfuegner13@gmail.com>
2018-04-10 14:42:23 -0500

    update 1

--- src/edu/ranken/cfuegner/HooRU.java
+++ src/edu/ranken/cfuegner/HooRU.java
@@ -1,5 +1,10 @@
 package edu.ranken.cfuegner;
 
 public class HooRU
-{
-}
+{ // START public class HooRU
+
+    // Global Constants
+    private static String ERROR_NAME_BLANK  = "Input invalid, Name cannot be left blank!";
+
+
+} // END public class HooRU
