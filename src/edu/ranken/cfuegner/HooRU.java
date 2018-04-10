d0979596faf6b34435e7920d824f9a7bfde8e576
Colin Fuegner <cfuegner13@gmail.com>
2018-04-10 14:44:58 -0500

    update 1 test

--- src/edu/ranken/cfuegner/HooRU.java
+++ src/edu/ranken/cfuegner/HooRU.java
@@ -6,5 +6,11 @@ public class HooRU
     // Global Constants
     private static String ERROR_NAME_BLANK  = "Input invalid, Name cannot be left blank!";
 
+    private static int MINYEAR              = 1900;     // Min year
+    private static int MAXYEAR              = 2018;     // Max year
+    private static int MINMONTH             = 1;        // Min month number
+    private static int MAXMONTH             = 12;       // Max month number
+    private static int MINDAY               = 1;        // Min day number
+
 
 } // END public class HooRU
