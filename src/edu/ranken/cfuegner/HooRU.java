2dd679a809021025547cd6f91baa47f05e993ce2
Brady Williams <williams.brady.a@gmail.com>
2018-04-11 15:55:49 -0500

    added findZodiacElement()

--- src/edu/ranken/cfuegner/HooRU.java
+++ src/edu/ranken/cfuegner/HooRU.java
@@ -865,6 +865,128 @@ public class HooRU
         return zodiacSign;
     }
 
+    // Determine Chinese zodiacElement
+    public static String findZodiacElement()
+    {
+        switch (userYear) {
+            // Wood
+            case 1984:
+            case 1925:
+            case 1985:
+            case 1974:
+            case 1975:
+            case 1964:
+            case 1965:
+            case 1954:
+            case 2014:
+            case 1955:
+            case 2015:
+            case 2004:
+            case 1945:
+            case 2005:
+            case 1934:
+            case 1994:
+            case 1935:
+            case 1995:
+                zodiacElement = "Wood";
+                break;
+            // Water
+            case 1972:
+            case 1973:
+            case 1962:
+            case 1963:
+            case 1952:
+            case 2012:
+            case 1953:
+            case 2013:
+            case 1942:
+            case 2002:
+            case 1943:
+            case 2003:
+            case 1932:
+            case 1992:
+            case 1933:
+            case 1993:
+            case 1982:
+            case 1983:
+                zodiacElement = "Water";
+                break;
+            // Metal
+            case 1960:
+            case 1961:
+            case 1950:
+            case 2010:
+            case 1951:
+            case 2011:
+            case 1940:
+            case 2000:
+            case 1941:
+            case 2001:
+            case 1990:
+            case 1931:
+            case 1991:
+            case 1920:
+            case 1980:
+            case 1981:
+            case 1970:
+            case 1971:
+                zodiacElement = "Metal";
+                break;
+            // Earth
+            case 1948:
+            case 2008:
+            case 1949:
+            case 2009:
+            case 1938:
+            case 1998:
+            case 1939:
+            case 1999:
+            case 1928:
+            case 1988:
+            case 1929:
+            case 1989:
+            case 1978:
+            case 1919:
+            case 1979:
+            case 1968:
+            case 1969:
+            case 1958:
+            case 2018:
+            case 1959:
+            case 2019:
+                zodiacElement = "Earth";
+                break;
+            // Fire
+            case 1936:
+            case 1996:
+            case 1937:
+            case 1997:
+            case 1926:
+            case 1986:
+            case 1927:
+            case 1987:
+            case 1976:
+            case 1977:
+            case 1966:
+            case 1967:
+            case 1956:
+            case 2016:
+            case 1957:
+            case 2017:
+            case 1946:
+            case 2006:
+            case 1947:
+            case 2007:
+                zodiacElement = "Fire";
+                break;
+            default:
+                zodiacElement = "?";
+                break;
+        }
+
+        return zodiacElement;
+    }
+
     public static void displayBirthDay()
     {
         String outputStr = "";
